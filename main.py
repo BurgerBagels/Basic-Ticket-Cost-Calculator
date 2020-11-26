ec713920215105dc1f7623cb8157c788fd048e94
George Martin <75093980+BurgerBagels@users.noreply.github.com>
2020-11-26 19:25:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ price = float(input("How much are tickets? (excluding service charge) £"))
 people = int(input("How many people are going? "))
 service = float(input("How much is the service charge? £"))
 
-ticket_cost = round(price * people, 2)
+ticket_cost = price * people
 service_charge = people * service
 total_amount = round(ticket_cost + service_charge, 2)
 
